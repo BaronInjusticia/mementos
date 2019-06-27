25ddaf33b00c517dc7903bb8a014c5b3b9c67c4d
BaronInjusticia <antonio.quetzatlcoatl@gmail.com>
2019-06-27 11:01:47 -0500

    commit antes de github

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function About() {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>Buscar Usuarios de github</p>
+      <p>Version 1.0</p>
+    </Fragment>
+  );
+}
+
+export default About;
---
+++ src/components/users/Users.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layouts/Spinner';
+import GithubContext from '../../context/github/githubContext';
+
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={usersStyle}>
+        {users.map(users => (
+          <UserItem key={users.id} users={users} />
+        ))}
+      </div>
+    );
+  }
+};
+
+const usersStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
+
+export default Users;
